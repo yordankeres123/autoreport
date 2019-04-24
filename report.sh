497a9022411f61cd646f54374499d7ba5002cb1a
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-25 02:28:57 +0700

    Add files via upload

---
+++ report.sh
@@ -0,0 +1,71 @@
+#!/system/xbin/bash
+#mau ngapain kontoll ??
+#ngerecode ?? gabakal jadiin lo maatah
+#ngereedit gabakal namalo tenar kontol
+#DICK YOU , DOG YOU ,FUCK YOU RECODE DICK FUCK
+clear
+Figlet Bigbos | lolcat
+echo "■■■■■■■■■■■■■■■■■" | lolcat
+echo "Report Facebook ■■" | lolcat
+echo "■■■■■■■■■■■■■■■■■" | lolcat
+echo
+echo "□□□□□□□□□□□□□□□□□□□□□□□□□□□□□" | lolcat
+echo "AUTHOR : BIGBOS STARLA" | lolcat
+echo "SOSMED : Bigbos Starla" | lolcat
+echo "team : CYB3R POP1CE +62" | lolcat
+echo "FRIENDS : ALL TEAM POP1CE" | lolcat
+echo "□□□□□□□□□□□□□□□□□□□□□□□□□□□□□" | lolcat
+echo
+echo "♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤♤" | lolcat
+echo "SILAHKAN TEKAN ANGKA 1 UNTUK LANJUT" | lolcat
+echo "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡" | lolcat
+echo
+echo "1. REACTION FACEBOOK" | lolcat
+echo "2. GAK ADA COK NTAR KENA MARAH" | lolcat
+echo "00. Kluar / Exi" | lolcat
+echo
+read -p "BigbosS~~##" v
+
+if [ $v = 1 ]
+then
+clear
+figler BIGBOS | lolcat
+sleep 2
+pkg install python2
+pkg install git
+pkg install figlet
+pkg install unzip
+pip2 install mechanize
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $v = 2 ]
+then
+clear
+figlet BIGBOS | lolcat
+sleep 2
+echo "ga ada goblok no 2 " | lolcat
+sleep 3
+echo "Jalankan lagi scriptnya" | lolcat
+exit
+fi
+
+if [ $v = 00 ]
+then
+clear
+figlet MAKASIH | lolcat
+sleep 2
+echo "MAKASIH TELAH MEMAKAI TOOLS KAMI" | lolcat
+sleep 1
+echo "KONTAK SAYA FB; Bigbos Starla" | lolcat
+sleep 1
+echo "CHANEL YOUTUBE SAYA" | lolcat
+sleep 1
+echo "Bigbos Starla Pliss Subscribe" | lolcat
+sleep 3
+echo "SAMPAI JUMPA LAGI" | lolcat
+exit
+fi
